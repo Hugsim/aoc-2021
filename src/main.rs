c72c55b424606458ecf2bff332bafe862a5877ea
Hugo Simonsson <simonssonhugo13@gmail.com>
2021-12-01 08:35:23 +0100

    Initial commit!

---
+++ src/main.rs
@@ -0,0 +1,16 @@
+#![allow(dead_code)]
+#![allow(non_snake_case)]
+
+mod days;
+mod util;
+
+fn main() {
+    let solutions = days::day1::solve();
+
+    if let Some(day1_solution) = solutions.0 {
+        println!("Part 1: {}", day1_solution);
+    }
+    if let Some(day2_solution) = solutions.1 {
+        println!("Part 2: {}", day2_solution);
+    }
+}
---
+++ src/util.rs
@@ -0,0 +1,53 @@
+use std::fs;
+use std::str::FromStr;
+
+pub fn read_file_to_string(file: &str) -> String {
+    fs::read_to_string(file).expect(&format!("Couldn't open {}", file)[..])
+}
+
+pub fn read_file_to_string_iter(file: &str) -> impl Iterator {
+    read_file_to_vec::<String>(file).into_iter()
+}
+
+pub fn read_file_to_vec<T>(file: &str) -> Vec<T>
+where
+    T: FromStr,
+    <T as FromStr>::Err: std::fmt::Debug,
+{
+    read_file_to_string(file)
+        .split('\n')
+        .into_iter()
+        .map(|x| x.parse::<T>().expect("Couldn't parse value in file"))
+        .collect()
+}
+
+pub fn sprint<T>(x: T)
+where
+    T: std::fmt::Display,
+{
+    println!("{}", x)
+}
+
+pub fn dprint<T>(x: T)
+where
+    T: std::fmt::Debug,
+{
+    println!("{:?}", x)
+}
+
+pub trait UnsafeParseable {
+    fn unsafe_parse<T>(self) -> T
+    where
+        T: FromStr,
+        <T as FromStr>::Err: std::fmt::Debug;
+}
+
+impl UnsafeParseable for &str {
+    fn unsafe_parse<T>(self) -> T
+    where
+        T: FromStr,
+        <T as FromStr>::Err: std::fmt::Debug,
+    {
+        self.parse::<T>().expect("Couldn't parse value")
+    }
+}
